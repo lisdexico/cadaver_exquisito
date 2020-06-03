acc31f2ef0d7ae9ae45f2146643fbc88940dfc53
Igna de la Roza <ignadelaroza@gmail.com>
2020-06-02 23:23:12 -0300

    Added search page that displays list of texts. Fixed create, which was only saving some data due to syntax errors in Scheema definitions.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var handlebars = require('handlebars');
 
 app.use(bodyparser.urlencoded({extended: true}));
 
+//---------------------------
+//------- Database ----------
+//---------------------------
+
 mongoose.connect('mongodb://localhost:27017/cadaveres',{useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
@@ -16,12 +20,12 @@ db.once('open', function(){
 var mainSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: {
-    Type: String
+    type: String
     //required: true,
     //maxlength: 100
   },
   description: {
-    Type: String
+    type: String
     //maxlength: 140
   },
   author: String,
@@ -58,23 +62,28 @@ var textSchema = mongoose.Schema({
 var main = mongoose.model('Main',mainSchema);
 var text = mongoose.model('Text',textSchema);
 
+//---------------------------
+//------- Webserver ---------
+//---------------------------
 
+// Use handlebars to render views and specify path to views
+app.set('views', __dirname + '/public/views')
+app.set('view engine', 'hbs')
+
+//-------- Home ----------
 app.get('/', (req,res)=> {
   res.sendFile(__dirname + '/public/home.html');
 });
 
+
+
+//-------- Create ----------
 app.get('/create', (req,res)=> {
   res.sendFile(__dirname + '/public/create.html');
 });
 
-app.get('/write/:id', (req,res)=> {
-  //meter handlebars para mandar id y para renderear el Parrafo
-  var lastparragraph = text.findOne({txt_id: req.params.id }.sort({ _id: -1 }).limit(1), function(err, user) {
-    if(err) console.log(err)});
-  res.sendFile(__dirname + '/public/write.html');
-});
-
 app.post('/create', (req,res)=> {
+
   var newText = new main({
     _id: new mongoose.Types.ObjectId(),
     title: req.body.title,
@@ -82,16 +91,25 @@ app.post('/create', (req,res)=> {
     author: req.body.author,
     pswd: req.body.pswd
   });
+
+  console.log(newText.title)
+  console.log(newText.description)
+  console.log(newText.author)
+  console.log(newText.description)
+
+
   newText.save((err)=>{
     if(err) throw err;
     console.log('Texto creado con exito');
     console.log(newText);
   });
+
   var newParrafo = new text ({
     _id: new mongoose.Types.ObjectId(),
     parrafo: req.body.parragraph,
     idText: newText._id
   });
+
   newParrafo.save((err)=>{
     if(err) throw err;
     console.log('nuevo parrafo añadido');
@@ -100,6 +118,19 @@ app.post('/create', (req,res)=> {
 
   res.sendFile(__dirname + '/public/home.html');
 });
+
+
+
+
+//-------- Write -----------
+
+app.get('/write/:id', (req,res)=> {
+  //meter handlebars para mandar id y para renderear el Parrafo
+  var lastparragraph = text.findOne({txt_id: req.params.id }.sort({ _id: -1 }).limit(1), function(err, user) {
+    if(err) console.log(err)});
+  res.sendFile(__dirname + '/public/write.html');
+});
+
 
 app.post('/write/:id', (req,res)=> {
   var newParrafo = new text ({
@@ -116,8 +147,21 @@ app.post('/write/:id', (req,res)=> {
   res.sendFile(__dirname + '/public/home.html');
 });
 
+
+
+//---------- Search -----------
 app.get('/search', (req,res)=> {
-  res.sendFile(__dirname + '/public/search.html');
+  //res.sendFile(__dirname + '/public/search.html');
+  //let word = "tuVieja"
+  //res.render("search", {word: word})
+  var texts
+  main.find()
+  .sort('-created')
+  .exec(function(err, texts) {
+    if (err) throw err;
+    console.log('\n\nfound:\n ' +  texts + '\n')
+    res.render('search', {texts: texts})
+  })
 });
 
 app.listen(3000, function () {
